e6ee2ef0eefbbae65e91888b18a59bab3d1f598f
Trujirendjj <juanj.trujillor@autonoma.edu.co>
2025-04-10 18:02:43 -0700

    Añadida la ventana infoConfiguracion, y servicio de sonidos creado

---
+++ src/services/sounds/PorscheSoundService.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services.sounds;
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author juanp
+ */
+public class PorscheSoundService implements SoundService{
+    public static final String ENCENDIDO_ROUTE = "src/assets/sounds/porscheSounds/encendido.wav";
+    @Override
+    public void encender() {
+        File sonido = new File(ENCENDIDO_ROUTE);
+        System.out.println(sonido.exists());
+        try{
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(sonido);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            while(!clip.isRunning()){
+                Thread.sleep(10);
+                System.out.println(clip.isRunning());
+            }
+            while(clip.isRunning()){
+                Thread.sleep(10);
+            }
+            clip.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+    @Override
+    public void apagar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void acelerar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void frenar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void frenarBruscamente() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/services/sounds/SoundService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package services.sounds;
+
+/**
+ *
+ * @author juanp
+ */
+public interface SoundService {
+    public void encender();
+    public void apagar();
+    public void acelerar();
+    public void frenar();
+    public void frenarBruscamente();
+}
